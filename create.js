2496515d661155c782ae974a4fe38cbfcb62e1be
Thomas <wyatsky@gmail.com>
2020-03-29 10:31:57 +1000

    Remove unused fail

--- create.js
+++ create.js
@@ -1,7 +1,6 @@
 import uuid from "uuid";
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
-import { fail } from "assert";
 
 
 export async function main(e, context, cb) {
